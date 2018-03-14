02158379cf2e92ce0f0b9bcd1aa05fdee89f78f1
Connor Prinster <mormonzephyr@bitbucket.org>
2018-03-14 07:51:56 -0600

    implemented more file-reading processes and will exit upon failure to open file

--- CS III/Hashing Poetry/main.cpp
+++ CS III/Hashing Poetry/main.cpp
@@ -2,12 +2,13 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <stdlib.h>
 
 #include "HashTable.h"
 
 void cleaningString(std::string & nextToken); //function for creating a string free of 
 											  //	special characters, punctuation, and uppercases
-std::string returnCleanStringFromFile(std::string filename);
+std::string returnCleanFileString(std::string filename);
 
 int main()
 {
@@ -15,12 +16,12 @@ int main()
 	//std::cout << "Enter filename to generate poem from: ";	//prompting user for filename
 	//std::cin >> filename;										//user inputs filename
 	filename = "green.txt"; //filename set for hardwiring testing
-	returnCleanStringFromFile(filename);
+	returnCleanFileString(filename);
 
 	std::cin.get();	//just so things work on my machine
 }
 
-std::string returnCleanStringFromFile(std::string filename)
+std::string returnCleanFileString(std::string filename)
 {
 	//-------------------------------------------------------
 	//checking that the file actually exists
@@ -29,33 +30,44 @@ std::string returnCleanStringFromFile(std::string filename)
 	std::string completeString = "";
 	std::cout << "Attempting to open " << filename << "...";
 	std::ifstream fin(filename);
-	if (fin)	std::cout << "SUCCESS" << std::endl;	//if 'fin' actually works, print success
-	else		std::cout << "FAILURE" << std::endl;	//else print failure
+	if (fin)
+	{
+		std::cout << "SUCCESS" << std::endl;	//if 'fin' actually works, print success
+	}
+	else
+	{
+		std::cout << "FAILURE -- EXITING PROGRAM \nPRESS ENTER TO EXIT" << std::endl; //else print failure
+		cin.get();
+		exit(1);
+	}
 	//-------------------------------------------------------
 	while (std::getline(fin, word))
 	{
-		completeVector.push_back(word);
+		completeVector.push_back(word);	//puts the current item into the vector
 	}
 	for (int i = 0; i < completeVector.size(); i++)
 	{
-		completeString += (completeVector[i] + " ");
+		completeString += (completeVector[i] + " "); //concatonates the word to the return string
 	}
+
+	cleaningString(completeString);
+
 	return completeString;
 }
 
-void cleaningString(std::string & nextWord)
+void cleaningString(std::string & token)
 {
-	for (int i = 0; i < nextWord.length();)
+	for (int i = 0; i < token.length();)
 	{
 		//checking if there is some non-alphanumeric characters, and if so, remove them
-		if (nextWord[i] > 255 || nextWord[i] < 0 || ispunct(nextWord[i]))
+		if (token[i] > 255 || token[i] < 0 || ispunct(token[i]))
 		{
-			nextWord.erase(i, 1);
+			token.erase(i, 1);
 		}
 		//regardless of if the specific char is already lowercase, make sure everything is lowercase
 		else
 		{
-			nextWord[i] = tolower(nextWord[i]);
+			token[i] = tolower(token[i]);
 			i++;
 		}
 	}
