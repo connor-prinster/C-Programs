dd20b018ea0092170da5a902167dbaf5cb908bf0
Connor Prinster <mormonzephyr@bitbucket.org>
2018-03-13 22:34:54 -0600

    really dug into getting a file into a string

--- CS III/Hashing Poetry/main.cpp
+++ CS III/Hashing Poetry/main.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #include "HashTable.h"
 
 void cleaningString(std::string & nextToken); //function for creating a string free of 
-															//	special characters, punctuation, and uppercases
+											  //	special characters, punctuation, and uppercases
+std::string returnCleanStringFromFile(std::string filename);
 
 int main()
 {
 	string filename = "";										//initializing variable filename
-	//===========================================================================================
-	//DEALING WITH OPENING THE FILE
-	
-	//----------------------
-	//hardwiring for testing purposes
-
-	//std::cout << "Enter filename to generate poem from: ";		//prompting user for filename
+	//std::cout << "Enter filename to generate poem from: ";	//prompting user for filename
 	//std::cin >> filename;										//user inputs filename
-	filename = "Seuss.txt";
-	std::cout << std::endl;	
-	//----------------------
+	filename = "green.txt"; //filename set for hardwiring testing
+	returnCleanStringFromFile(filename);
 
+	std::cin.get();	//just so things work on my machine
+}
+
+std::string returnCleanStringFromFile(std::string filename)
+{
 	//-------------------------------------------------------
 	//checking that the file actually exists
+	std::string word;
+	vector <std::string> completeVector;
+	std::string completeString = "";
 	std::cout << "Attempting to open " << filename << "...";
 	std::ifstream fin(filename);
 	if (fin)	std::cout << "SUCCESS" << std::endl;	//if 'fin' actually works, print success
-	else		std::cout << "FAILURE" << std::endl;	//else pring failure
+	else		std::cout << "FAILURE" << std::endl;	//else print failure
 	//-------------------------------------------------------
-	//===========================================================================================
-
-	std::cin.get();	//just so things work on my machine
+	while (std::getline(fin, word))
+	{
+		completeVector.push_back(word);
+	}
+	for (int i = 0; i < completeVector.size(); i++)
+	{
+		completeString += (completeVector[i] + " ");
+	}
+	return completeString;
 }
 
 void cleaningString(std::string & nextWord)
